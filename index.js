4d17759e6b6540606712205c8e70268b150e8831
Jimer Samuel Espinoza <ejimersamuel@yahoo.com>
2022-10-18 21:48:22 -0500

    1.0.1

--- index.js
+++ index.js
@@ -3,5 +3,34 @@ console.log("Hello World");
 var variable = "Hola";
 console.log(variable);
 
-var numero = 5;
-console.log(numero);
+var numer0 = 5;
+console.log(numer0);
+
+var compa = true;
+console.log(compa);
+
+const constante = 5;
+console.log(constante);
+
+numer0 = false;
+console.log(numer0);
+
+// constante = 8;
+// console.log(constante);
+
+var numero1 = 5;
+var numero2 = 6;
+var respuesta = numero1 + numero2;
+console.log("La respuesta es: " + respuesta);
+
+//potencia
+var numero1 = 2;
+var numero2 = 3;
+var respuesta = Math.pow(numero1, numero2);
+console.log("La respuesta es: " + respuesta);
+
+//oparacion combinada
+var numero1 = 2;
+var numero2 = 3;
+var respuesta = Math.pow(numero1, 4) - numero1 / (numero2 + 45);
+console.log("La respuesta es: " + respuesta);
